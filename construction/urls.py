bb8dfe4b733f7ed507f666d6157c616b6a281f0b
Salonce <jakukaj@gmail.com>
2022-08-17 21:19:48 +0200

    template authentication

--- construction/urls.py
+++ construction/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='index'),
+  path('', views.home, name='home'),
   path('inventoryBrowser/', views.inventoryBrowser, name='inventoryBrowser'),
   path('housePlanBrowser/', views.housePlanBrowser, name='housePlanBrowser'),
   path('housePlanBrowser/<int:id>/', views.housePlan, name='housePlan'),
